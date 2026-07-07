41bc88daa6377554c04724ffe4aee68134b04b4c
Renan <renanfcunha@gmail.com>
2026-07-07 13:08:13 -0300

    Criação da pagina Home e componentes Navbar e Footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+function Navbar() {
+
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+                            bg-green-800 text-white'>
+
+                <div className='container flex justify-between text-lg mx-8'>
+                    Farmacia React
+
+                    <div className='flex gap-4'>
+                        Home
+                        Categorias
+
+                    </div>
+                </div>
+            </div >
+        </>
+    )
+}
+
+export default Navbar
